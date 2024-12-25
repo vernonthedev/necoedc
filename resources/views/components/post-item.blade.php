9a7f66f4f05b799db17aba975735009a27be89ca
joshuamumbere <joshuamumbere71@gmail.com>
2024-12-25 21:13:55 +0300

    Modified and added contents to the blogs

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,4 +1,27 @@
 
+<div class="blog-item">
+    <div class="blog-image">
+        <img src="storage/{{ $post->thumbnail }}" alt="image">
+    </div>
+
+    <div class="blog-content">
+        <div class="bmeta">
+            <span>
+                <i class="bx bx-time-five"></i> 27 Jan , 2024
+            </span>
+
+            <span class="bcat">
+                <a href="#">Education</a>
+            </span>
+        </div>
+
+        <h3><a href="blog-details.html">{{ $post->title }}</a></h3>
+        <p>
+            {{ $post->shortBody() }}
+        </p>
+        <a href="{{ route('blog.show', $post) }}" class="bbtn">Explore More</a>
+    </div>
+</div>
 
 {{ $post->thumbnail }}
 
@@ -8,4 +31,4 @@
 ​{{$post->title}}
 {{ $post->shortBody()}}
 {{ route('blog.show', $post)}}
-     
+
