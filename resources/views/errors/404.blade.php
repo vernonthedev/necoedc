67677889f6d7ce5b0b8ed155525ca039f1288bbb
vernonthedev <techjaja2@gmail.com>
2024-12-25 23:59:38 +0300

    Added the 404 page content integration.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,19 @@
-<x-app-layout meta-title="" meta-description="">
+<x-app-layout meta-title="Page Not Found | NeCo EDC " meta-description="">
+		<!-- Start page_not_found -->
+		<section class="page_not_found">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 text-center wow fadeIn">
+						<img src="{{ asset('assets/img/404.svg')}}" alt="404"> 
+						<h2>Page not found: /error</h2>
+						<p>Please try searching for some other page.</p>
+						<a href="{{route('home')}}" class="bg_btn bt">Back To Home</a>
+					</div> <!-- End Col -->
+
+				</div>
+			</div>
+		</section>
+		<!-- End page_not_found -->
 
 </x-app-layout>
 
